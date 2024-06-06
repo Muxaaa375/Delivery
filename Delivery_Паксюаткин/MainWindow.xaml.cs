4cb5da2738428d05f7f5df6854ec8a8f6ee28902
20rwplay@gmail.com <20rwplay@gmail.com>
2024-06-06 17:58:21 +0500

    Разработанная страница MainWindow. Паксюаткин

--- Delivery_Паксюаткин/MainWindow.xaml.cs
+++ Delivery_Паксюаткин/MainWindow.xaml.cs
@@ -1,28 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Delivery_Паксюаткин
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
         public MainWindow()
         {
             InitializeComponent();
+            OpenPage(new Pages.Login.Main());
+            init = this;
+        }
+
+        public void OpenPage(Page Page)
+        {
+            frame.Navigate(Page);
         }
     }
 }
